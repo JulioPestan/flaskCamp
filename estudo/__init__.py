35a14e5af31e4db049482a220cd5a9f0366f772f
JulioPestan <pestanabikuri@hotmail.com>
2024-03-31 23:28:28 -0300

    nova rota, import SQLAlquemy, flask_migrate, criação dos models,configuração de conexão com bd, criação do database, criação do arquivo wsgi.py

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.bd'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from estudo.views import index
+from estudo.models import Contato 
--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,10 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/nova/')
+def novapag():
+    return 'Outra rota'
