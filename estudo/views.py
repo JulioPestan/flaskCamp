d6f4b6ffe44277dfccb1793f7fa2f5841a66bd53
JulioPestan <pestanabikuri@hotmail.com>
2024-04-04 00:43:42 -0300

    Criei o contato.html, dentro dele criei dois formulários, um com methods="GET" para testar e outro com methods="POST" para testar e gravar os dados no banco. Criei a route('/contato/') para renderizar a página de contato. Importei o request do flask na view para receber os dados das requisições dos formulários e testei para ver como eles funcionam. Importei o bd na view para fazer as sessions de add e commit para gravar dados no banco. Só lembrando que essa não é maneira segura de criar formulários no flask. Vide o próximo commit.

--- estudo/views.py
+++ estudo/views.py
@@ -1,10 +1,38 @@
-from estudo import app
-from flask import render_template, url_for
+from estudo import app, db  #--> importei o db para gravar dados no banco de dados 
+from flask import render_template, url_for,request #--> usei o request para pegar dados do formulário, GET e POST
+
+from estudo.models import Contato #--> importei a classe Contato para instaciar ela, criei um objeto para adicionar no banco
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/nova/')
+#criação de uma rota que recebe requisições GET e POST 
+@app.route('/contato/', methods=['GET','POST'])
 def novapag():
-    return 'Outra rota'
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        print('GET', pesquisa)
+        context.update({'pesquisa':pesquisa})
+    
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+        
+
+        contato = Contato(
+            nome = nome,
+            email = email,
+            assunto = assunto,
+            mensagem = mensagem
+        )
+
+        db.session.add(contato) 
+        db.session.commit()
+
+    return render_template('contato.html', context = context)
+
+    
