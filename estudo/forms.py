3e394d95a00196c1e8cca5ac77b59af4a0fb50bc
JulioPestan <pestanabikuri@hotmail.com>
2024-04-07 11:19:47 -0300

    Nesse commit vou criar o formulário de cadastro do usuário.
    Mas antes, preciso fazer uma coisa que esqueci de fazer no commit anterior que foi atualizar o banco, pois  criei uma tabela User e não salvei. No terminal dois comando resolvem isso: primeiro 'flask db migrate' e depois 'flask db upgrade'.
    Feito isso, vou para o arquivo e noto que o formulário de UserForm é bem parecido com o do ContatoForm.
    A diferença principal é o campo senha que tenho que ter atenção. Começo importando o que vai ser usado, que é PasswordField do wtforms, o EqualTo e ValidationError que vão comparar a senha de confirmação com a digitada e criar uma validação de campo de e-mail para saber se o e-mail já está cadastrado no banco, respectivamente.
    A função que salva o formulário, tem um ponto especial que é a senha.
    Para validar o e-mail criei a função validade_email() que vai cuidar disso. Não esquecer de importar o User.
    Mais uma importação importante é o bcrypt, que vai ser usado para criptografar a senha antes de gravar no banco.

--- estudo/forms.py
+++ estudo/forms.py
@@ -1,9 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
-from estudo.models import Contato
-from estudo import db
+from estudo.models import Contato, User
+from estudo import db, bcrypt
+
+class UserForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Sobrenome', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(),Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmacao_senha = PasswordField('Senha', validators=[DataRequired(), EqualTo('senha')])
+    btnSubmit = SubmitField('Cadastrar')
+
+    def validade_email(self, email):
+        if User.query.filter(email=email.data).first():
+            return ValidationError('Usuário já cadastrado com esse e-mail!!!')
+        
+    def save(self):
+        senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
+        user = user(
+            nome = self.nome.data,
+            sobrenome = self.sobrenome.data,
+            email = self.email.data,
+            senha = senha
+        )
+
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+        
+
 
 
 class ContatoForm(FlaskForm):
